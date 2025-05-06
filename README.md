a2cbc6f7c4a74e1cd8cbb9a437d9a63282384f97
rohanmad <rohanmadan2027@gmail.com>
2025-05-05 23:30:58 -0700

    explore done

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Lab 5 - Starter
 Worked on solo by Rohan Madan.
+Expose.html: https://rohanmad.github.io/Lab5_Starter/expose.html
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,74 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  let voices = [];
+  function populateVoiceList() {
+    if (typeof speechSynthesis === "undefined") {
+      return;
+    }
+    
+
+    voices = speechSynthesis.getVoices();
+  
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+  
+      if (voices[i].default) {
+        option.textContent += " — DEFAULT";
+      }
+  
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      option.value = voices[i].name;
+      document.getElementById("voice-select").appendChild(option);
+    }
+  }
+  
+  populateVoiceList();
+  if (
+    typeof speechSynthesis !== "undefined" &&
+    speechSynthesis.onvoiceschanged !== undefined
+  ) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+
+  const dropdown = document.querySelector("#voice-select");
+  let voiceoption = "";
+  dropdown.addEventListener("change", (event)=>{
+    voiceoption = dropdown.value;
+    console.log(voiceoption);
+  })
+
+  const txtbox = document.querySelector("#text-to-speak");
+  let text = "";
+  const synth = window.speechSynthesis;
+
+
+  txtbox.addEventListener("input", (event)=>{
+    text = txtbox.value;
+  });
+
+  
+  const btn = document.querySelector("#explore button");
+  btn.addEventListener("click", (event)=>{
+    let utterance = new SpeechSynthesisUtterance(text);
+    const selectedVoice = speechSynthesis.getVoices().find(voice => voice.name === voiceoption);
+    utterance.voice = selectedVoice;
+    console.log(selectedVoice);
+    synth.speak(utterance);
+  })
+
+  function isTalking() {
+    let speaking = document.querySelector("#explore img");
+    if (speechSynthesis.speaking) {
+      speaking.src = "assets/images/smiling-open.png";
+    } else{
+      speaking.src = "assets/images/smiling.png";
+    }
+    requestAnimationFrame(isTalking); 
+  }
+
+  isTalking();
 }
